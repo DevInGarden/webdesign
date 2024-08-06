a4ff364ef1a6cdbc0610eb5978832733655d5c90
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-08-06 14:57:45 +0900

    Add files via upload

--- 4/javascript/script.js
+++ 4/javascript/script.js
@@ -16,4 +16,20 @@ jQuery(document).ready(function () {
         $(".slidelist").delay(2000);
     });
 
+    $(function () {
+        $(".tabmenu>li>a").click(function () {
+            $(this).parent().addClass("active")
+                .siblings()
+                .removeClass("active")
+            return false;
+        });
+    });
+
+    $(".notice li:first").click(function () {
+        $("#modal").addClass("active");
+    });
+    $(".btn").click(function () {
+        $("#modal").removeClass("active");
+    });
+
 });
