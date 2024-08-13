4f27b42a43466b928736c1395dcc6555249ed5a3
DevInGarden <160681245+DevInGarden@users.noreply.github.com>
2024-08-13 15:49:47 +0900

    Add files via upload

--- 7/javascript/script.js
+++ 7/javascript/script.js
@@ -16,5 +16,21 @@ jQuery(document).ready(function () {
             .appendTo(".imgslide")
     }, 3000)
 
+    $(function () {
+        $(".tabmenu>li>a").click(function () {
+            $(this).parent()
+                .addClass("active")
+                .siblings()
+                .removeClass("active");
+            return false;
+        });
+    });
+
+    $(".notice li:first").click(function () {
+        $("#modal").addClass("active");
+    });
+    $(".btn").click(function () {
+        $("#modal").removeClass("active");
+    });
 
 });
