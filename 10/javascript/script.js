5db839ecf15353009577b71860f73031fa9b673a
jwkim97211 <lkwe998@naver.com>
2024-08-15 14:24:24 +0900

    chap10

---
+++ 10/javascript/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function () {
+
+    setInterval(function () {
+        $(".slidelist").delay(2000);
+        $(".slidelist").animate({ marginLeft: '-100%' })
+        $(".slidelist").delay(2000);
+        $(".slidelist").animate({ marginLeft: '-200%' })
+        $(".slidelist").delay(2000);
+        $(".slidelist").animate({ marginLeft: 0 })
+        $(".slidelist").delay(2000);
+    })
+
+    $(".navi>li").mouseover(function () {
+        $(this).find(".submenu").stop().slideDown();
+    }).mouseout(function () {
+        $(this).find(".submenu").stop().slideUp();
+    });
+
+    $(".notice li:first").click(function () {
+        $("#modal").addClass("active")
+    });
+    $(".btn").click(function () {
+        $("#modal").removeClass("active")
+    });
+});
